6b2f60130ba8f5be75a673d1fdf3b6b1fbebcdb3
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2025-02-06 18:44:46 -0500

    Create Article entity

---
+++ lib/features/news/domain/article.dart
@@ -0,0 +1,48 @@
+import 'package:equatable/equatable.dart';
+
+class Article extends Equatable {
+  const Article({
+    required this.author,
+    required this.title,
+    required this.description,
+    required this.url,
+    required this.imageUrl,
+    required this.publishedAt,
+    required this.content,
+    required this.source,
+  });
+
+  final String author;
+  final String title;
+  final String description;
+  final String url;
+  final String imageUrl;
+  final DateTime publishedAt;
+  final String content;
+  final Source source;
+
+  @override
+  List<Object?> get props => [
+        author,
+        title,
+        description,
+        url,
+        imageUrl,
+        publishedAt,
+        content,
+        source,
+      ];
+}
+
+class Source extends Equatable {
+  const Source({
+    required this.id,
+    required this.name,
+  });
+
+  final String id;
+  final String name;
+
+  @override
+  List<Object?> get props => [id, name];
+}
